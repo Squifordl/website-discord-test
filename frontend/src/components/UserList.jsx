d62a3d36edf70c9bcb32766765fe110ffb1ac45b
Squiford <62345804+Squifordl@users.noreply.github.com>
2023-07-12 19:57:03 -0300

    Update UserList.jsx

--- frontend/src/components/UserList.jsx
+++ frontend/src/components/UserList.jsx
@@ -9,7 +9,6 @@ function UserList() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const [users, setUsers] = useState([]);
-  const [showMessage, setShowMessage] = useState(false);
   const [showPopup, setShowPopup] = useState(false);
   const [selectedUserId, setSelectedUserId] = useState(null);
   const history = useHistory();
@@ -37,19 +36,6 @@ function UserList() {
     fetchUsers();
   }, []);
 
-  useEffect(() => {
-    const checkUser = async () => {
-      const user_check = localStorage.getItem("userId");
-      if (!user_check) {
-        setShowMessage(true);
-        setTimeout(() => {
-          history.push("/");
-        }, 3000);
-      }
-    };
-    checkUser();
-  }, [history]);
-
   const handleAvatarClick = (userId) => {
     setSelectedUserId(userId);
     setShowPopup(true);
@@ -61,63 +47,54 @@ function UserList() {
 
   return (
     <div className="user-list-container">
-      {showMessage ? (
-        <div className="loading-container">
-          <div className="spinner"></div>
-          <div className="redirect-message">Você não está logado, estou lhe redirecionando</div>
-        </div>
-      ) : (
-        <>
-          <h2 className="user-list-title">Usuários cadastrados</h2>
-          <div className="user-list">
-            {isLoading ? (
-              <div className="loading-container">
-                <div className="spinner"></div>
-                <div className="loading-message">Carregando usuários...</div>
-              </div>
-            ) : error ? (
-              <div className="error-message">Erro ao carregar usuários: {error}</div>
-            ) : (
-              users.map((user, index) => (
-                <div key={index} className="user-list-item">
-                  <div
-                    className="user-avatar-container"
-                    onClick={() => handleAvatarClick(user.userId)}
-                    role="button"
-                    aria-label="Abrir perfil de usuário"
-                  >
-                    <img
-                      src={user.avatarUrl}
-                      alt={`Avatar do usuário ${user.username}`}
-                      className="user-avatar"
-                    />
-                    <i className="fas fa-eye icon-eye"></i>
-                  </div>
-                  <p className="user-name">{user.username}</p>
-                  <button
-                    className="user-profile-btn"
-                    onClick={() => handleUserProfileClick(user.userId)}
-                    aria-label="Abrir perfil de usuário"
-                  >
-                    Ver perfil
-                  </button>
-                </div>
-              ))
-            )}
-            {showPopup && (
-              <div className="popup">
-                <div className="popup-content">
-                  <h3>Deseja ser redirecionado para o perfil do usuário?</h3>
-                  <button onClick={() => setShowPopup(false)}>Cancelar</button>
-                  <button onClick={() => (window.location.href = `https://discord.com/users/${selectedUserId}`)}>
-                    Ir para o perfil
-                  </button>
-                </div>
-              </div>
-            )}
+      <h2 className="user-list-title">Usuários cadastrados</h2>
+      <div className="user-list">
+        {isLoading ? (
+          <div className="loading-container">
+            <div className="spinner"></div>
+            <div className="loading-message">Carregando usuários...</div>
           </div>
-        </>
-      )}
+        ) : error ? (
+          <div className="error-message">Erro ao carregar usuários: {error}</div>
+        ) : (
+          users.map((user, index) => (
+            <div key={index} className="user-list-item">
+              <div
+                className="user-avatar-container"
+                onClick={() => handleAvatarClick(user.userId)}
+                role="button"
+                aria-label="Abrir perfil de usuário"
+              >
+                <img
+                  src={user.avatarUrl}
+                  alt={`Avatar do usuário ${user.username}`}
+                  className="user-avatar"
+                />
+                <i className="fas fa-eye icon-eye"></i>
+              </div>
+              <p className="user-name">{user.username}</p>
+              <button
+                className="user-profile-btn"
+                onClick={() => handleUserProfileClick(user.userId)}
+                aria-label="Abrir perfil de usuário"
+              >
+                Ver perfil
+              </button>
+            </div>
+          ))
+        )}
+        {showPopup && (
+          <div className="popup">
+            <div className="popup-content">
+              <h3>Deseja ser redirecionado para o perfil do usuário?</h3>
+              <button onClick={() => setShowPopup(false)}>Cancelar</button>
+              <button onClick={() => (window.location.href = `https://discord.com/users/${selectedUserId}`)}>
+                Ir para o perfil
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
