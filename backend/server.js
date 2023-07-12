af24abe6627ac8c57fed83317ea9cbe4b8dcc565
Squiford <62345804+Squifordl@users.noreply.github.com>
2023-07-12 00:02:12 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -139,7 +139,7 @@ app.get("/api/users", async (req, res) => {
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  res.status(404).json({ message: "Route not found" });
 });
 
 app.listen(process.env.PORT, () =>
