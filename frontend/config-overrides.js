efdb5c716041706241281ccd04ffae5e640cc7ab
Squiford <62345804+Squifordl@users.noreply.github.com>
2023-07-12 01:48:41 -0300

    as

--- frontend/config-overrides.js
+++
@@ -1,8 +0,0 @@
-module.exports = function override(config, env) {
-    config.resolve.fallback = {
-        ...config.resolve.fallback,
-        crypto: require.resolve('crypto-browserify'),
-    };
-
-    return config;
-}
